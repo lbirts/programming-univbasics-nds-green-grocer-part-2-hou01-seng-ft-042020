b04e57be3b041d3951cdeaec88cd789fbb232204
Lauren Birts <birtslauren@gmail.com>
2020-03-31 04:47:45 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,6 +4,22 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+   item_index = 0
+  while cart[item_index] do
+    item = cart[item_index]
+    coupon_item = find_item_by_name_in_collection(item[:item], coupons)
+    if coupon_item
+      item[:count] -= coupon_item[:num]
+      cart << {
+        :item => "#{coupon_item[:item]} W/COUPON",
+        :price => (coupon_item[:cost] / coupon_item[:num]),
+        :clearance => item[:clearance],
+        :count => coupon_item[:num]
+      }
+    end
+    item_index += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
