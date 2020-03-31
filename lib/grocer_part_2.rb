025ce815efc2c5faee4d5e29edda39cfbeee0977
Lauren Birts <birtslauren@gmail.com>
2020-03-31 04:48:06 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -47,4 +47,18 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  consolidated_cart = consolidate_cart(cart)
+  coupon_applied_cart = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(coupon_applied_cart)
+  total = 0
+  item_index = 0
+  while final_cart[item_index] do
+    item = final_cart[item_index]
+    total += item[:price] * item[:count]
+    item_index += 1
+  end
+  if total >= 100
+    total -= total * 0.1
+  end
+  total
 end
